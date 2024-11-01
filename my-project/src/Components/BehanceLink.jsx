555708959b9a923a1a3a1dd425d44d01980ca1f7
yazceline <celineterrado@gmail.com>
2024-11-02 07:55:33 +0800

    update

--- my-project/src/Components/BehanceLink.jsx
+++ my-project/src/Components/BehanceLink.jsx
@@ -13,7 +13,7 @@ const BehanceLink = () => {
       onClick={openLinkInNewTab}
       className="bg-gradient-to-r from-purple-400 to-purple-600 hover:from-purple-600 hover:to-purple-800 text-white font-raleway font-normal py-3 px-6 rounded-[24px] md:w-auto w-full"
     >
-      BEHANCE PROFILE
+      BEHANCE PORTFOLIO
     </button>
   );
 };
