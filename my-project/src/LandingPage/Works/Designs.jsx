843aebdd29383454869ad3e3a1d8e83abef20c39
yazceline <celineterrado@gmail.com>
2024-11-02 16:20:22 +0800

    latest update

--- my-project/src/LandingPage/Works/Designs.jsx
+++ my-project/src/LandingPage/Works/Designs.jsx
@@ -39,10 +39,84 @@ const Designs = () => {
             </div>
           </div>
         </div>
+{/* ------------------------------------------------------------- */}
+
+<div class="max-w-screen-xl px-4 mx-auto grid gap-8 md:grid-cols-2">
+          <div className=" flex flex-col text-white rounded-[24px] mb-4 md:flex md:items-left md:justify-left ">
+            <ImageModal src="/Images/DesignImages/LoyaltyTXT.png" alt="LoyaltyTXT" />
+            <div class="py-4">
+              <h2 className="mt-2 text-lg font-bold ">LoyaltyTXT</h2>
+              <ViewButton />
+            </div>
+          </div>
+
+          {/* ---------------------- */}
+          <div className=" flex flex-col text-white rounded-[24px] mb-4 md:flex md:items-left md:justify-left ">
+            <ImageModal src="/Images/DesignImages/Swiss.png" alt="Switzerland" />
+            <div class="py-4">
+              <h2 className="mt-2 text-lg font-bold ">Exploring Switzerland</h2>
+              <ViewButton />
+            </div>
+          </div>
+        </div>
+
+
+
+
+
+        {/* ------------------------------------------------------------- */}
+
+<div class="max-w-screen-xl px-4 mx-auto grid gap-8 md:grid-cols-2">
+          <div className=" flex flex-col text-white rounded-[24px] mb-4 md:flex md:items-left md:justify-left ">
+            <ImageModal src="/Images/DesignImages/CB.png" alt="CyberRebirth" />
+            <div class="py-4">
+              <h2 className="mt-2 text-lg font-bold ">CyberRebirth</h2>
+              <ViewButton />
+            </div>
+          </div>
+
+          {/* ---------------------- */}
+          <div className=" flex flex-col text-white rounded-[24px] mb-4 md:flex md:items-left md:justify-left ">
+            <ImageModal src="/Images/DesignImages/Astoria.png" alt="Astoria" />
+            <div class="py-4">
+              <h2 className="mt-2 text-lg font-bold ">Hotel Astoria</h2>
+              <ViewButton />
+            </div>
+          </div>
+        </div>
+
+
+{/* 
+        ---------------------------------------------- */}
+
+
+<div class="max-w-screen-xl px-4 mx-auto grid gap-8 md:grid-cols-2">
+          <div className=" flex flex-col text-white rounded-[24px] mb-4 md:flex md:items-left md:justify-left ">
+            <ImageModal src="/Images/DesignImages/DH.png" alt="DisputeHelp" />
+            <div class="py-4">
+              <h2 className="mt-2 text-lg font-bold ">DisputeHelp</h2>
+              <ViewButton />
+            </div>
+          </div>
+
+          {/* ---------------------- */}
+          <div className=" flex flex-col text-white rounded-[24px] mb-4 md:flex md:items-left md:justify-left ">
+            <ImageModal src="/Images/DesignImages/PizzaPlace.png" alt="PizzaPlace" />
+            <div class="py-4">
+              <h2 className="mt-2 text-lg font-bold ">That Pizza Place</h2>
+              <ViewButton />
+            </div>
+          </div>
+        </div>
+
+
+{/* 
+        ---------------------------------------------- */}
+
 
         <div class="max-w-screen-xl px-4 mx-auto grid gap-8 md:grid-cols-2">
           <div className=" flex flex-col text-white rounded-[24px] mb-4 md:flex md:items-left md:justify-left ">
-            <ImageModal src="/Images/DesignImages/juice2.png" alt="Karmine" />
+            <ImageModal src="/Images/DesignImages/juice2.png" alt="Juice" />
             <div class="py-4">
               <h2 className="mt-2 text-lg font-bold ">JuiceLabs</h2>
               <ViewButton />
@@ -51,21 +125,18 @@ const Designs = () => {
 
           {/* ---------------------- */}
           <div className=" flex flex-col text-white rounded-[24px] mb-4 md:flex md:items-left md:justify-left ">
-            <ImageModal src="/Images/DesignImages/car2.png" alt="Karmine" />
+            <ImageModal src="/Images/DesignImages/car2.png" alt="Luminaire" />
             <div class="py-4">
               <h2 className="mt-2 text-lg font-bold ">Luminaire</h2>
               <ViewButton />
             </div>
           </div>
         </div>
-      
 
-      <div className=" pb-12 pt-8">
-        <FigmaLink2/>
+        <div className=" pb-12 pt-8">
+          <FigmaLink2 />
         </div>
-
       </div>
-    
     </div>
   );
 };
