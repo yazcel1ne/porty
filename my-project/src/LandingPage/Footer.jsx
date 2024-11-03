c0922f8bf29f32a1883745aa128d5895d7537771
yazceline <celineterrado@gmail.com>
2024-11-03 18:47:37 +0800

    contact fix

--- my-project/src/LandingPage/Footer.jsx
+++ my-project/src/LandingPage/Footer.jsx
@@ -3,32 +3,26 @@ import WavyBg from "./Pages/WavyBg";
 
 const Footer = () => {
   return (
-    <div class="relative">
-
-    <footer className="max-w-screen-xl px-4 my-10  mx-auto grid gap-8 md:grid-cols-1">
-      <div className="container mx-auto p-4 pt-6 md:p-6">
-        <div className="flex justify-center mb-4">
-          <div className="flex items-center">
-          <img
-              src="/Images/logo5.png"
-              alt="Logo"
-              class="self-center text-2xl font-semibold whitespace-nowrap text-white"
-            />     
-                   
+    <div className="relative">
+      <footer className="max-w-screen-xl px-4 my-2 mx-auto grid gap-8 md:grid-cols-1">
+        <div className="container mx-auto p-4 pt-6 md:p-6">
+          <div className="flex justify-center mb-4">
+            <div className="flex items-center">
+              <img
+                src="/Images/logo5.png"
+                alt="Logo"
+                className="self-center text-2xl font-semibold whitespace-nowrap text-white"
+              />
+            </div>
+            <div className="flex items-center"></div>
           </div>
-          <div className="flex items-center">
-         
+          <div>
+            <p className="text-center text-white">
+              ©Copyright. All Rights Reserved
+            </p>
           </div>
         </div>
-       
- 
-
-        <div className="border-b-2 border-gradient-b mb-4" />
-   
-
-
-      </div>
-    </footer>
+      </footer>
     </div>
   );
 };
