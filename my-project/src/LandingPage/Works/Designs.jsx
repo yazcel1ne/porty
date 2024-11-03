b7b47b00b747fda1e5f6221e2e34bf27e8dc4cd7
yazceline <celineterrado@gmail.com>
2024-11-03 18:23:18 +0800

    deseign update

--- my-project/src/LandingPage/Works/Designs.jsx
+++ my-project/src/LandingPage/Works/Designs.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import DesignHeader from "../../assets/DesignHeader";
-import ViewButton from "../../Components/ViewButton";
-import ViewMore from "../../Components/ViewMore";
-import { BouncingImage, BouncingSmall } from "../../assets/BouncingBall";
 import ImageModal from "./DesignComps.jsx/ImageModal";
 import FigmaLink from "../../Components/FigmaLink";
-import FigmaLink2 from "../../Components/FigmaLink2";
 import BehanceLink from "../../Components/BehanceLink";
 
 const Designs = () => {
@@ -16,150 +12,37 @@ const Designs = () => {
       </div>
 
       <div className="relative-container ">
-        {/* Grid container for the first set of images */}
-        <div className="max-w-screen-xl px-4 mx-auto grid gap-0 md:gap-8 md:grid-cols-3">
-          <div className="flex flex-col relative text-white rounded-[24px] mb-8 overflow-hidden">
-            <ImageModal src="/Images/DesignImages/karmine2.png" alt="Karmine" />
-            <div
-              className="absolute top-0 left-0 w-full glassmorphism2
-             p-2 text-center"
+        {/* Grid container for the images */}
+        <div className="max-w-screen-xl px-4 mx-auto grid gap-4 md:gap-8 md:grid-cols-3">
+          {[
+            { src: "/Images/DesignImages/karmine2.png", alt: "Karmine", title: "Karmine" },
+            { src: "/Images/DesignImages/plant2.png", alt: "Oasis", title: "Oasis" },
+            { src: "/Images/DesignImages/LoyaltyTXT.png", alt: "LoyaltyTXT", title: "LoyaltyTXT" },
+            { src: "/Images/DesignImages/Swiss.png", alt: "Switzerland", title: "Exploring Switzerland" },
+            { src: "/Images/DesignImages/CB.png", alt: "CB", title: "CyberRebirth" },
+            { src: "/Images/DesignImages/Astoria.png", alt: "Astoria", title: "Hotel Astoria" },
+            { src: "/Images/DesignImages/DH.png", alt: "DisputeHelp", title: "DisputeHelp" },
+            { src: "/Images/DesignImages/PizzaPlace.png", alt: "PizzaPlace", title: "That Pizza Place" },
+            { src: "/Images/DesignImages/juice2.png", alt: "Juice", title: "JuiceLabs" },
+            { src: "/Images/DesignImages/car2.png", alt: "Luminaire", title: "Luminaire" },
+            { src: "/Images/DesignImages/lux.png", alt: "Lux", title: "LuxWatch" },
+            { src: "/Images/DesignImages/Urban.png", alt: "UrbanVibe", title: "UrbanVibe" },
+          ].map((image, index) => (
+            <div 
+              key={index} 
+              className="flex flex-col relative text-white rounded-[24px] overflow-hidden shadow-2xl" // Changed to shadow-2xl here
             >
-              <h2 className="text-lg font-semibold">Karmine</h2>
+              <ImageModal src={image.src} alt={image.alt} />
+              <div className="absolute top-0 left-0 w-full glassmorphism2 p-2 text-center">
+                <h2 className="text-lg font-semibold">{image.title}</h2>
+              </div>
             </div>
-          </div>
-
-          <div className="flex flex-col relative text-white rounded-[24px] mb-8 overflow-hidden">
-            <ImageModal src="/Images/DesignImages/plant2.png" alt="Oasis" />
-            <div
-              className="absolute -0 left-0 w-full glassmorphism2
-             p-2 text-center"
-            >
-              <h2 className="text-lg font-semibold">Oasis</h2>
-            </div>
-          </div>
-
-          <div className="flex flex-col relative text-white rounded-[24px] mb-8 overflow-hidden">
-            <ImageModal
-              src="/Images/DesignImages/LoyaltyTXT.png"
-              alt="LoyaltyTXT"
-            />
-            <div
-              className="absolute -0 left-0 w-full glassmorphism2
-             p-2 text-center"
-            >
-              <h2 className="text-lg font-semibold">LoyaltyTXT</h2>
-            </div>
-          </div>
-        </div>
-
-        {/* Additional grid containers */}
-        <div className="max-w-screen-xl px-4 mx-auto grid gap-0 md:gap-8 md:grid-cols-3">
-          <div className="flex flex-col relative text-white rounded-[24px] mb-8 overflow-hidden">
-            <ImageModal
-              src="/Images/DesignImages/Swiss.png"
-              alt="Switzerland"
-            />
-            <div
-              className="absolute -0 left-0 w-full glassmorphism2
-             p-2 text-center"
-            >
-              <h2 className="text-lg font-semibold">Exploring Switzerland</h2>
-            </div>
-          </div>
-
-          <div className="flex flex-col relative text-white rounded-[24px] mb-8 overflow-hidden">
-            <ImageModal src="/Images/DesignImages/CB.png" alt="CB" />
-            <div
-              className="absolute -0 left-0 w-full glassmorphism2
-             p-2 text-center"
-            >
-              <h2 className="text-lg font-semibold">CyberRebirth</h2>
-            </div>
-          </div>
-
-          <div className="flex flex-col relative text-white rounded-[24px] mb-8 overflow-hidden">
-            <ImageModal src="/Images/DesignImages/Astoria.png" alt="Astoria" />
-            <div
-              className="absolute -0 left-0 w-full glassmorphism2
-             p-2 text-center"
-            >
-              <h2 className="text-lg font-semibold">Hotel Astoria</h2>
-            </div>
-          </div>
-        </div>
-
-        {/* Repeat similar structure for additional grids */}
-        <div className="max-w-screen-xl px-4 mx-auto grid gap-0 md:gap-8 md:grid-cols-3">
-          <div className="flex flex-col relative text-white rounded-[24px] mb-8 overflow-hidden">
-            <ImageModal src="/Images/DesignImages/DH.png" alt="DisputeHelp" />
-            <div
-              className="absolute -0 left-0 w-full glassmorphism2
-             p-2 text-center"
-            >
-              <h2 className="text-lg font-semibold">DisputeHelp</h2>
-            </div>
-          </div>
-
-          <div className="flex flex-col relative text-white rounded-[24px] mb-8 overflow-hidden">
-            <ImageModal
-              src="/Images/DesignImages/PizzaPlace.png"
-              alt="PizzaPlace"
-            />
-            <div
-              className="absolute -0 left-0 w-full glassmorphism2
-             p-2 text-center"
-            >
-              <h2 className="text-lg font-semibold">That Pizza Place</h2>
-            </div>
-          </div>
-
-          <div className="flex flex-col relative text-white rounded-[24px] mb-8 overflow-hidden">
-            <ImageModal src="/Images/DesignImages/juice2.png" alt="Juice" />
-            <div
-              className="absolute -0 left-0 w-full glassmorphism2
-             p-2 text-center"
-            >
-              <h2 className="text-lg font-semibold">JuiceLabs</h2>
-            </div>
-          </div>
-        </div>
-
-        <div className="max-w-screen-xl px-4 mx-auto grid gap-0 md:gap-8 md:grid-cols-3">
-          <div className="flex flex-col relative text-white rounded-[24px] mb-8 overflow-hidden">
-            <ImageModal src="/Images/DesignImages/car2.png" alt="Luminaire" />
-            <div
-              className="absolute -0 left-0 w-full glassmorphism2
-             p-2 text-center"
-            >
-              <h2 className="text-lg font-semibold">Luminaire</h2>
-            </div>
-          </div>
-
-          <div className="flex flex-col relative text-white rounded-[24px] mb-8 overflow-hidden">
-            <ImageModal src="/Images/DesignImages/lux.png" alt="Lux" />
-            <div
-              className="absolute -0 left-0 w-full glassmorphism2
-             p-2 text-center"
-            >
-              <h2 className="text-lg font-semibold">LuxWatch</h2>
-            </div>
-          </div>
-
-          <div className="flex flex-col relative text-white rounded-[24px] mb-8 overflow-hidden">
-            <ImageModal src="/Images/DesignImages/Urban.png" alt="UrbanVibe" />
-            <div
-              className="absolute -0 left-0 w-full glassmorphism2
-             p-2 text-center"
-            >
-              <h2 className="text-lg font-semibold">UrbanVibe</h2>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
 
       {/* Footer section with links */}
       <div className="flex justify-center w-full pb-16 pt-16">
-
         <div className="flex flex-col md:flex-row space-x-0 md:space-x-4 space-y-4 md:space-y-0 w-full max-w-md p-4 md:p-0">
           <div className="flex-shrink-0 w-full md:w-auto whitespace-nowrap">
             <BehanceLink />
