d667b1c28bf7d18619d938774eddb1feb3bcc0e3
yazceline <celineterrado@gmail.com>
2024-11-05 23:39:37 +0800

    hotfix

--- my-project/src/LandingPage/Hero.jsx
+++ my-project/src/LandingPage/Hero.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import NavBar from "./NavBar";
-import {
-  BouncingImage,
-  BouncingSmall,
-  BouncingMed,
-  BouncingL,
-} from "../assets/BouncingBall";
 import "/src/styles/transitions.css";
-import WavyBg from "./Pages/WavyBg";
-import GradientText from "../assets/GradientText";
 import FigmaLink from "../Components/FigmaLink";
 import BehanceLink from "../Components/BehanceLink";
-import Futuristic from "./Pages/Futuristic";
+import HeroAnim from "/src/assets/HeroAnim.json";
+import Ballanim from "/src/assets/Ballanim.json";
+import uiuxanim from "/src/assets/uiuxanim.json";
+import lappy2 from "/src/assets/lappy2.json";
+import { Player } from "@lottiefiles/react-lottie-player"; // Import the Player component
 
 const Hero = () => {
   const downloadPdf = () => {
@@ -47,7 +43,14 @@ const Hero = () => {
                 <p className="max-w-2xl mb-4 mt-4 font-light text-white lg:mb-12 md:text-lg lg:text-xl dark:text-gray-400 relative z-10 typing-animation">
                   Frontend Developer | WordPress Developer
                 </p>
+
+                <p className="mb-4 mt-4 font-light text-gray-200 lg:mb-12 relative z-10 ">
+                Designing Exciting and Engaging Web Experiences,  Driven by Passion and a Love for Creativity!
+                </p>
+               
               </div>
+
+             
             </div>
 
             <div className="flex flex-col sm:flex-row gap-4">
@@ -57,13 +60,37 @@ const Hero = () => {
           </div>
 
           {/* Adjust the image for different screen sizes */}
-          <div className="lg:flex lg:col-span-5 lg:mt-0 lg:justify-center lg:items-center">
+          <div className="lg:flex lg:col-span-5 lg:mt-0 lg:justify-center lg:items-center relative">
             <img
               src="/Images/pic6.png"
               alt="bg"
-              className="hidden lg:block w-full h-auto object-cover lg:max-w-[400px] xl:max-w-[500px]"
+              className="hidden lg:block w-full h-auto object-cover lg:max-w-[400px] xl:max-w-[500px] flip-in-animation"
+            />
+
+            {/* Add bouncing designer palette */}
+            {/* <img
+              src="/Images/TechLogos/Palette.png"
+              alt="Designer Palette"
+              className="hidden lg:block absolute top-[75%] left-[10%] w-16 h-16 bounce-animation"
+              style={{ animationDelay: "0.2s" }}
+            /> */}
+
+            {/* Add bouncing keyboard */}
+            {/* <img
+              src="/Images/TechLogos/Keyboard.png"
+              alt="Keyboard"
+              className="hidden lg:block absolute top-[90%] left-[30%] w-18 h-18 bounce-animation"
+              style={{ animationDelay: "0.4s" }}
+            /> */}
+            <Player
+              autoplay
+              loop
+              src={lappy2}
+              className="hidden lg:block absolute top-[60%]  right-[40%] w-15 h-15 opacity-75" // Adjust opacity as needed
+              style={{ animationDelay: "3s" }}
             />
           </div>
+          
         </div>
       </section>
     </>
