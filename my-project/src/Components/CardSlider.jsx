133b2ae8872008048447917ae5836d9697a2bee9
yazceline <celineterrado@gmail.com>
2024-11-07 15:13:23 +0800

    update

--- my-project/src/Components/CardSlider.jsx
+++ my-project/src/Components/CardSlider.jsx
@@ -9,9 +9,8 @@ const CardSlider = () => {
       </div>
       <div className="relative   font-raleway ">
         <div class="relative-container">
-         
           <div class="max-w-screen-xl px-4 mx-auto grid gap-8 md:gap-4 lg:gap-8 md:grid-cols-3">
-            <div className="glassmorphism flex-col text-gray-300 mb-4 md:flex md:items-left md:justify-left transition-transform duration-300 transform rounded-[24px] shadow-lg py-12">
+            <div className="glassmorphism flex-col text-gray-300  md:flex md:items-left md:justify-left transition-transform duration-300 transform rounded-[24px] shadow-lg py-12">
               {" "}
               <div class="font-raleway flex flex-col flex-grow pl-8 pr-8">
                 <div class="text-white text-4xl mb-2">
@@ -36,13 +35,11 @@ const CardSlider = () => {
                   <span class="text-yellow-500">&#9733;</span>
                   <span class="text-yellow-500">&#9733;</span>
                 </div>
-
-                <div class="flex-grow"></div>
               </div>
             </div>
             {/* ---------------------- */}
 
-            <div className="glassmorphism flex-col text-gray-300 mb-4 md:flex md:items-left md:justify-left transition-transform duration-300 transform rounded-[24px] shadow-lg py-12">
+            <div className="glassmorphism flex-col text-gray-300 md:flex md:items-left md:justify-left transition-transform duration-300 transform rounded-[24px] shadow-lg py-12">
               {" "}
               <div class="font-raleway flex flex-col flex-grow pl-8 pr-8">
                 <div class="text-white text-4xl mb-2">
@@ -68,12 +65,107 @@ const CardSlider = () => {
                   <span class="text-yellow-500">&#9733;</span>
                   <span class="text-yellow-500">&#9733;</span>
                 </div>
+              </div>
+            </div>
+
+            <div className="glassmorphism flex-col text-gray-300  md:flex md:items-left md:justify-left transition-transform duration-300 transform rounded-[24px] shadow-lg py-12">
+              {" "}
+              <div class="font-raleway flex flex-col flex-grow pl-8 pr-8">
+                <div class="text-white text-4xl mb-2">
+                  <span>&#8220;</span>
+                </div>
+
+                <p>
+                  "I collaborated with Celine on some design and graphic work
+                  for my business, and I couldn't be happier with the results.
+                  Celine is incredibly talented and brings a unique creative
+                  vision to each project. She works quickly without sacrificing
+                  quality, which made the entire process smooth and stress-free.
+                  "
+                </p>
+
+                <h1 class="font-semibold text-white pt-4">
+                  Lisa S.
+                  <p class="font-light">Entrepreneur,PMU</p>
+                </h1>
+
+                <div class="flex ">
+                  <span class="text-yellow-500">&#9733;</span>
+                  <span class="text-yellow-500">&#9733;</span>
+                  <span class="text-yellow-500">&#9733;</span>
+                  <span class="text-yellow-500">&#9733;</span>
+                  <span class="text-yellow-500">&#9733;</span>
+                </div>
 
                 <div class="flex-grow"></div>
               </div>
             </div>
 
-            <div className="glassmorphism flex-col text-gray-300 mb-4 md:flex md:items-left md:justify-left transition-transform duration-300 transform rounded-[24px] shadow-lg py-12">
+            <div className="glassmorphism flex-col text-gray-300  md:flex md:items-left md:justify-left transition-transform duration-300 transform rounded-[24px] shadow-lg py-12">
+              {" "}
+              <div class="font-raleway flex flex-col flex-grow pl-8 pr-8">
+                <div class="text-white text-4xl mb-2">
+                  <span>&#8220;</span>
+                </div>
+
+                <p>
+                  Celine is exceptionally talented and impressively quick in her
+                  work. Her creativity and efficiency made the entire process
+                  seamless and enjoyable. I highly recommend her to anyone
+                  looking for top-notch design delivered with speed and
+                  professionalism.
+                </p>
+
+                <h1 class="font-semibold text-white pt-4">
+                  Sam L.
+                  <p class="font-light">Business Owner</p>
+                </h1>
+
+                <div class="flex ">
+                  <span class="text-yellow-500">&#9733;</span>
+                  <span class="text-yellow-500">&#9733;</span>
+                  <span class="text-yellow-500">&#9733;</span>
+                  <span class="text-yellow-500">&#9733;</span>
+                  <span class="text-yellow-500">&#9733;</span>
+                </div>
+              </div>
+            </div>
+
+            {/* ------------------------------------------------------------- */}
+
+            <div className="glassmorphism flex-col text-gray-300  md:flex md:items-left md:justify-left transition-transform duration-300 transform rounded-[24px] shadow-lg py-12">
+              {" "}
+              <div class="font-raleway flex flex-col flex-grow pl-8 pr-8">
+                <div class="text-white text-4xl mb-2">
+                  <span>&#8220;</span>
+                </div>
+
+                <p>
+                  "I’ve had the pleasure of working with Celine, and I can’t
+                  recommend her enough. She has a great eye for design and
+                  always manages to create websites that are not only beautiful
+                  but also easy to use. Her attention to detail and thoughtful
+                  approach really stand out in every project."
+                </p>
+
+                <h1 class="font-semibold text-white pt-4">
+                  Sheena C.
+                  <p class="font-light">UI/UX Designer</p>
+                </h1>
+
+                <div class="flex ">
+                  <span class="text-yellow-500">&#9733;</span>
+                  <span class="text-yellow-500">&#9733;</span>
+                  <span class="text-yellow-500">&#9733;</span>
+                  <span class="text-yellow-500">&#9733;</span>
+                  <span class="text-yellow-500">&#9733;</span>
+                </div>
+
+                <div class="flex-grow"></div>
+              </div>
+            </div>
+
+            <div className="glassmorphism flex-col text-gray-300  md:flex md:items-left md:justify-left transition-transform duration-300 transform rounded-[24px] shadow-lg py-12">
               {" "}
               <div class="font-raleway flex flex-col flex-grow pl-8 pr-8">
                 <div class="text-white text-4xl mb-2">
@@ -99,10 +191,9 @@ const CardSlider = () => {
                   <span class="text-yellow-500">&#9733;</span>
                   <span class="text-yellow-500">&#9733;</span>
                 </div>
-
-                <div class="flex-grow"></div>
               </div>
             </div>
+
             <div className></div>
           </div>
 
