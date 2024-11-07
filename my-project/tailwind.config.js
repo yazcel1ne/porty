aa12255201b8d55cb01fb99e21a9fbdda1f7aa24
yazceline <celineterrado@gmail.com>
2024-11-08 06:37:50 +0800

    update

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}","./node_modules/flowbite/**/*.js"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       backgroundImage: (theme) => ({
-        "gradient-custom":
-          "linear-gradient(90deg, #2B1848 0%, #191919 52%, #141414 100%)",
-          
+        "gradient-custom": "linear-gradient(90deg, #2B1848 0%, #191919 52%, #141414 100%)",
       }),
       fontFamily: {
         raleway: ["Poppins", "sans-serif"],
@@ -15,11 +13,11 @@ export default {
         purpbg: "#2E233B",
       },
       animation: {
-
         gradient: 'animatedgradient 6s ease infinite alternate',
-
       },
-   
+      cursor: {
+        custom: 'url(/Images/pointer.png "), auto', // Add your custom cursor here
+      },
     },
   },
   plugins: [require("flowbite/plugin")],
