89d1e92658c7d5fb9f4f8e055ab24b7483ed9d2e
yazceline <celineterrado@gmail.com>
2024-11-26 20:04:57 +0800

    fix

--- my-project/src/LandingPage/Hero.jsx
+++ my-project/src/LandingPage/Hero.jsx
@@ -3,82 +3,68 @@ import NavBar from "./NavBar";
 import "/src/styles/transitions.css";
 import FigmaLink from "../Components/FigmaLink";
 import BehanceLink from "../Components/BehanceLink";
-import HeroAnim from "/src/assets/HeroAnim.json";
-import Ballanim from "/src/assets/Ballanim.json";
-import uiuxanim from "/src/assets/uiuxanim.json";
+
 import lappy2 from "/src/assets/lappy2.json";
 import { Player } from "@lottiefiles/react-lottie-player"; // Import the Player component
 
 const Hero = () => {
-  const downloadPdf = () => {
-    window.open(
-      "https://drive.google.com/file/d/1s3rTegsEIkH2P7YuZQUwW9jmeznJPkX5/view?usp=sharing",
-      "_blank"
-    );
-  };
-
   return (
     <>
       <NavBar />
 
       <section className="bg-transparent">
-        <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 mt-10 sm:mt-20">
-          <div className="mr-auto place-self-center lg:col-span-7">
-            <div className="max-w-2xl mb-4 animate-left">
-              <h1 className="text-3xl sm:text-4xl md:text-5xl xl:text-6xl font-bold tracking-tight leading-none text-white pt-10 sm:pt-20">
-                Hi!
-              </h1>
-
-              <h1
-                className="text-3xl sm:text-4xl md:text-5xl xl:text-6xl font-bold mb-4 tracking-tight leading-none text-white pt-4"
-                style={{ animationDelay: "0.5s" }}
-              >
-                I'm Celine
-              </h1>
-              <div>
-                <img
-                  src="/Images/TechLogos/Palette.png"
-                  alt="Designer Palette"
-                  className="hidden lg:block absolute top-[20%] left-[20%] w-10 h-10 bounce-animation"
-                  style={{ animationDelay: "0.2s" }}
-                />
-                <span className="text-3xl sm:text-4xl md:text-5xl xl:text-6xl font-bold bg-gradient-to-r from-purple-700 via-pink-500 to-blue-400 text-transparent bg-clip-text animate-gradient">
-                  Web Designer
-                </span>
+        <div className="grid max-w-screen-xl px-4 mx-auto lg:gap-8 xl:gap-0  lg:grid-cols-12 min-h-screen ">
+          {/* Left Content */}
+          <div className="mr-auto place-self-center lg:col-span-7 ">
+            <div className="flex items-center justify-center ">
+              <div className="max-w-2xl mb-4 animate-left">
+                {/* Headings */}
+                <h1 className="text-2xl sm:text-4xl md:text-4xl lg:text-5xl xl:text-6xl font-bold tracking-tight leading-snug text-white ">
+                  Hi!
+                </h1>
+                <h1 className="text-2xl sm:text-4xl md:text-4xl lg:text-5xl xl:text-6xl font-bold tracking-tight leading-snug text-white pt-4 pb-4">
+                  I'm Celine
+                </h1>
                 <div>
-                  <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-2xl max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl mb-4 mt-4 font-light text-white lg:mb-12 dark:text-gray-400 relative z-10 typing-animation break-words">
-                    Frontend Developer | WordPress Developer
-                  </p>
+                  <img
+                    src="/Images/TechLogos/Palette.png"
+                    alt="Designer Palette"
+                    className="hidden lg:block absolute top-[2%] left-[20%] w-10 h-10 bounce-animation"
+                    style={{ animationDelay: "0.2s" }}
+                  />
+                  <span className="text-3xl sm:text-4xl md:text-5xl xl:text-6xl font-bold bg-gradient-to-r from-purple-700 via-pink-500 to-blue-400 text-transparent bg-clip-text animate-gradient">
+                    Web Designer
+                  </span>
                 </div>
 
-                <p className="mb-4 mt-4 font-light text-gray-200 lg:mb-12 relative z-10 ">
+                {/* Paragraph */}
+                <p className="text-base sm:text-lg md:text-xl lg:text-2xl xl:text-2xl max-w-xs sm:max-w-md md:max-w-lg lg:max-w-2xl mb-4 mt-4 font-light text-white lg:mb-12 dark:text-gray-400 relative z-10 typing-animation break-words">
+                  Frontend Developer | WordPress Developer
+                </p>
+                <p className=" font-light text-gray-200 lg:mb-4 ">
                   I Design Innovative and Engaging Web Experiences Driven by
-                  Passion and a Love for Creativity. 😎{" "}
+                  Passion and a Love for Creativity. 😎
                 </p>
               </div>
             </div>
-
             <div className="flex flex-col sm:flex-row gap-4">
               <BehanceLink />
               <FigmaLink />
             </div>
           </div>
 
-          {/* Adjust the image for different screen sizes */}
+          {/* Right Content */}
           <div className="lg:flex lg:col-span-5 lg:mt-0 lg:justify-center lg:items-center relative">
             <img
               src="/Images/pic6.png"
               alt="bg"
-              className="hidden lg:block w-full h-auto object-cover lg:max-w-[400px] xl:max-w-[500px] flip-in-animation"
+              className="hidden lg:block w-full h-auto object-cover lg:max-w-[300px] xl:max-w-[400px] flip-in-animation"
             />
-
-      
             <Player
               autoplay
               loop
               src={lappy2}
-              className="hidden lg:block absolute top-[60%]  right-[40%] w-15 h-15 opacity-75" // Adjust opacity as needed
-              style={{ animationDelay: "3s" }}
+              className="hidden lg:block absolute top-[50%] right-[40%] w-[300px] h-[250px] opacity-75"
             />
           </div>
         </div>
